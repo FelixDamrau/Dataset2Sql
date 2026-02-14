03b04d60c6a2e80a9efeca478211166274d99f7b
Felix Damrau <mail@felixdamrau.de>
2026-02-14 12:05:50 +0100

    Update to .NET10 (#1)
    * Update to .NET10
    * Make sure this may work on non-win machines
    * Be a good boy and sanatize the dataset names
    * Refactor and separate UI and SQL stuff
    * Update pipeline to net10
    * Add PR Pipeline

--- DatabaseSettings.cs
+++ DatabaseSettings.cs
@@ -2,7 +2,7 @@ namespace Develix.Dataset2Sql;
 
 public class DatabaseSettings
 {
-    public string Server { get; set; } = ".";
+    public string Server { get; set; } = "localhost";
     public string Name { get; set; } = "";
     public string Username { get; set; } = "";
     public string Password { get; set; } = "";
--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
-﻿using System.Data;
+using System.Data;
 using Microsoft.Data.SqlClient;
-using System.Text;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
-using System.Data.Common;
 
 namespace Develix.Dataset2Sql;
 
@@ -23,7 +21,14 @@ public class Program
 
 
         Console.Write("Enter the path to the XML file: ");
-        var xmlFilePath = Console.ReadLine()?.Trim('"') ?? throw new InvalidOperationException("Invalid file path");
+        var inputPath = Console.ReadLine() ?? throw new InvalidOperationException("Invalid input");
+        var xmlFilePath = inputPath.Trim('"', '\'');
+
+        if (!File.Exists(xmlFilePath))
+        {
+            Console.WriteLine($"Error: File not found at '{xmlFilePath}'");
+            return;
+        }
 
         Console.Write($"Enter DB Name [{dbSettings.Name}]: ");
         var dbNameInput = Console.ReadLine();
@@ -42,7 +47,10 @@ public class Program
             var dataSet = new DataSet();
             dataSet.ReadXml(xmlFilePath);
 
-            ImportDatasetToSqlServer(dataSet, connectionStringBuilder, dbName);
+            using var commandBuilder = new SqlCommandBuilder();
+            var importer = new DatasetImporter(commandBuilder);
+
+            importer.ImportDatasetToSqlServer(dataSet, connectionStringBuilder, dbName, ConfirmDatabaseDrop);
 
             Console.WriteLine("Import completed successfully!");
         }
@@ -57,107 +65,17 @@ public class Program
         Console.ReadKey();
     }
 
-    private static void ImportDatasetToSqlServer(DataSet dataSet, SqlConnectionStringBuilder connectionStringBuilder, string dbName)
+    private static bool ConfirmDatabaseDrop(string dbName)
     {
-        var connectionString = connectionStringBuilder.ToString();
-        var masterConnectionString = new SqlConnectionStringBuilder(connectionString) { InitialCatalog = "master" }.ToString();
-
-        CreateDatabase(dbName, masterConnectionString);
-
-        using SqlConnection connection = new(connectionString);
-        connection.Open();
-
-        foreach (DataTable table in dataSet.Tables)
+        Console.WriteLine($"DB {dbName} already exists. Database must be dropped press 'y' to continue: ");
+        var response = Console.ReadLine();
+        if (!"y".Equals(response, StringComparison.OrdinalIgnoreCase))
         {
-            CreateTable(table, connection);
-            ImportTableData(table, connection);
+            Console.WriteLine("Tschüss.");
+            Environment.Exit(0);
+            return false;
         }
-    }
-
-    private static void CreateDatabase(string dbName, string masterConnectionString)
-    {
-        using SqlConnection masterConnection = new(masterConnectionString);
-        masterConnection.Open();
-
-        var checkDbQuery = $"SELECT COUNT(*) FROM sys.databases WHERE name = '{dbName}'";
-        var checkDbCmd = new SqlCommand(checkDbQuery, masterConnection);
-        var dbCount = (int)checkDbCmd.ExecuteScalar();
-
-        if (dbCount > 0)
-        {
-            Console.WriteLine($"DB {dbName} already exists. Database must be dropped press 'y' to continue: ");
-            var response = Console.ReadLine();
-            if (!"y".Equals(response, StringComparison.OrdinalIgnoreCase))
-            {
-                Console.WriteLine("Tschüss.");
-                Environment.Exit(0);
-            }
-
-            var dropDbQuery = $"ALTER DATABASE [{dbName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE [{dbName}]";
-            var dropDbCmd = new SqlCommand(dropDbQuery, masterConnection);
-            dropDbCmd.ExecuteNonQuery();
-        }
-
-        var createDbQuery = $"CREATE DATABASE [{dbName}]";
-        var createDbCmd = new SqlCommand(createDbQuery, masterConnection);
-        createDbCmd.ExecuteNonQuery();
-
-        Console.WriteLine($"Database '{dbName}' created.");
-    }
-
-    private static void CreateTable(DataTable table, SqlConnection connection)
-    {
-        var createTableQuery = new StringBuilder();
-        createTableQuery.AppendLine($"CREATE TABLE [{table.TableName}] (");
-
-        for (var i = 0; i < table.Columns.Count; i++)
-        {
-            var column = table.Columns[i];
-            var sqlType = GetSqlType(column.DataType);
-
-            createTableQuery.Append($"    [{column.ColumnName}] {sqlType}");
-
-            if (i < table.Columns.Count - 1)
-                createTableQuery.AppendLine(",");
-            else
-                createTableQuery.AppendLine();
-        }
-
-        createTableQuery.AppendLine(")");
-
-        using SqlCommand command = new(createTableQuery.ToString(), connection);
-        command.ExecuteNonQuery();
-        Console.WriteLine($"Table '{table.TableName}' created.");
-    }
-
-    private static string GetSqlType(Type dotNetType)
-    {
-        return dotNetType switch
-        {
-            _ when dotNetType == typeof(string) => "NVARCHAR(MAX)",
-            _ when dotNetType == typeof(int) => "INT",
-            _ when dotNetType == typeof(long) => "BIGINT",
-            _ when dotNetType == typeof(decimal) => "DECIMAL(18, 6)",
-            _ when dotNetType == typeof(double) => "FLOAT",
-            _ when dotNetType == typeof(float) => "REAL",
-            _ when dotNetType == typeof(DateTime) => "DATETIME",
-            _ when dotNetType == typeof(bool) => "BIT",
-            _ when dotNetType == typeof(byte) => "TINYINT",
-            _ when dotNetType == typeof(Guid) => "UNIQUEIDENTIFIER",
-            _ => "NVARCHAR(MAX)"
-        };
-    }
-
-    private static void ImportTableData(DataTable table, SqlConnection connection)
-    {
-        using SqlBulkCopy bulkCopy = new(connection);
-        bulkCopy.DestinationTableName = table.TableName;
-
-        foreach (DataColumn column in table.Columns)
-            bulkCopy.ColumnMappings.Add(column.ColumnName, column.ColumnName);
-
-        bulkCopy.WriteToServer(table);
-        Console.WriteLine($"Imported {table.Rows.Count} rows to table '{table.TableName}'.");
+        return true;
     }
 
     static void ShowVersionScreen()
