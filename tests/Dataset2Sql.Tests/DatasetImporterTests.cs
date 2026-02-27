c33afa94385d118cc2d933fcb2c5cb998867ef1b
Felix Damrau <mail@felixdamrau.de>
2026-02-27 22:07:04 +0100

    Add tests as punishment for failed builds

---
+++ tests/Dataset2Sql.Tests/DatasetImporterTests.cs
@@ -0,0 +1,25 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Develix.Dataset2Sql.Tests;
+
+public class DatasetImporterTests
+{
+    [Test]
+    [Arguments("master")]
+    [Arguments("MASTER")]
+    [Arguments("MaStEr")]
+    [Arguments("model")]
+    [Arguments("msdb")]
+    [Arguments("tempdb")]
+    public async Task ImportDatasetToSqlServer_WhenTargetIsSystemDatabase_ThenReturnsFalse(string dbName)
+    {
+        var commandBuilder = new SqlCommandBuilder();
+        var importer = new DatasetImporter(commandBuilder);
+        var dataSet = new DataSet();
+
+        var result = importer.ImportDatasetToSqlServer(dataSet, [], dbName, confirmDropCallback: _ => true);
+
+        await Assert.That(result).IsFalse();
+    }
+}
